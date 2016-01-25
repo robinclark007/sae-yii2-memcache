564ed9d7cec9c704998088fedfdf1787086aa481
R B <deng@RdeMacBook-Air.local>
2016-01-25 23:53:33 +0800

    add SaeMemCache.php

---
+++ SaeMemCache.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\caching;
+
+use yii\base\InvalidConfigException;
+
+/**
+ * MemCache implements a cache application component based on [memcache](http://pecl.php.net/package/memcache)
+ * and [memcached](http://pecl.php.net/package/memcached).
+ *
+ * MemCache supports both [memcache](http://pecl.php.net/package/memcache) and
+ * [memcached](http://pecl.php.net/package/memcached). By setting [[useMemcached]] to be true or false,
+ * one can let MemCache to use either memcached or memcache, respectively.
+ *
+ *
+ * Note, there is no security measure to protected data in memcache.
+ * All data in memcache can be accessed by any process running in the system.
+ *
+ * To use MemCache as the cache application component, configure the application as follows,
+ * SAE memcache(http://www.sinacloud.com/doc/sae/php/memcache.html)
+ * ~~~
+ * [
+ *     'components' => [
+ *         'cache' => [
+ *             'class' => 'yii\caching\SaeMemCache',
+ *         ],
+ *     ],
+ * ]
+ * ~~~
+ *
+ * @author tyan <tyan4g@gmail.com>
+ * @since 2.0
+ */
+class SaeMemCache extends Cache
+{
+    /**
+     * @var \Memcache|\Memcached the Memcache instance
+     */
+    private $_cache;
+
+    /**
+     * Initializes this application component.
+     * It creates the memcache instance and adds memcache servers.
+     */
+    public function init()
+    {
+        parent::init();
+        $this->_cache = new \Memcache;
+        $this->_cache->connect();
+    }
+    /**
+     * Retrieves a value from cache with a specified key.
+     * This is the implementation of the method declared in the parent class.
+     * @param string $key a unique key identifying the cached value
+     * @return string|boolean the value stored in cache, false if the value is not in the cache or expired.
+     */
+    protected function getValue($key)
+    {
+        return $this->_cache->get($key);
+    }
+
+    /**
+     * Retrieves multiple values from cache with the specified keys.
+     * @param array $keys a list of keys identifying the cached values
+     * @return array a list of cached values indexed by the keys
+     */
+    protected function getValues($keys)
+    {
+        return $this->_cache->get($keys);
+    }
+
+    /**
+     * Stores a value identified by a key in cache.
+     * This is the implementation of the method declared in the parent class.
+     *
+     * @param string $key the key identifying the value to be cached
+     * @param string $value the value to be cached
+     * @param integer $duration the number of seconds in which the cached value will expire. 0 means never expire.
+     * @return boolean true if the value is successfully stored into cache, false otherwise
+     */
+    protected function setValue($key, $value, $duration)
+    {
+        $expire = $duration > 0 ? $duration + time() : 0;
+
+        return $this->_cache->set($key, $value, 0, $expire);
+    }
+
+    /**
+     * Stores multiple key-value pairs in cache.
+     * @param array $data array where key corresponds to cache key while value is the value stored
+     * @param integer $duration the number of seconds in which the cached values will expire. 0 means never expire.
+     * @return array array of failed keys. Always empty in case of using memcached.
+     */
+    protected function setValues($data, $duration)
+    {
+
+        return parent::setValues($data, $duration);
+    }
+
+    /**
+     * Stores a value identified by a key into cache if the cache does not contain this key.
+     * This is the implementation of the method declared in the parent class.
+     *
+     * @param string $key the key identifying the value to be cached
+     * @param string $value the value to be cached
+     * @param integer $duration the number of seconds in which the cached value will expire. 0 means never expire.
+     * @return boolean true if the value is successfully stored into cache, false otherwise
+     */
+    protected function addValue($key, $value, $duration)
+    {
+        $expire = $duration > 0 ? $duration + time() : 0;
+
+        return $this->_cache->add($key, $value, 0, $expire);
+    }
+
+    /**
+     * Deletes a value with the specified key from cache
+     * This is the implementation of the method declared in the parent class.
+     * @param string $key the key of the value to be deleted
+     * @return boolean if no error happens during deletion
+     */
+    protected function deleteValue($key)
+    {
+        return $this->_cache->delete($key, 0);
+    }
+
+    /**
+     * Deletes all values from cache.
+     * This is the implementation of the method declared in the parent class.
+     * @return boolean whether the flush operation was successful.
+     */
+    protected function flushValues()
+    {
+        return $this->_cache->flush();
+    }
+}
